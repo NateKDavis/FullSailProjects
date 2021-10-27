b4d38a92193b92d3f02348579f0c2550a6e7ce94
Nathan Davis <NateDavis697@gmail.com>
2021-10-27 12:33:08 -0500

    Completed Labs

---
+++ Blackjack/BlackjackLibrary/Hand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BlackjackLibrary.ICard;
+
+namespace BlackjackLibrary
+{
+    public class Hand
+    {
+        protected List<ICard> _cards = new List<ICard>();
+
+        public virtual void AddCard(ICard card)
+        {
+            _cards.Add(card);
+        }
+
+        public virtual void Draw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            int idx = 0;
+            foreach (ICard item in _cards)
+            {
+                if (idx % 7 == 0)
+                {
+                    Console.WriteLine();
+                }
+                
+                item.Draw(x + (12 * idx), y);
+                idx++;
+            }
+        }
+    }
+}
